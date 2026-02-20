1d0caf5481fd8fcecdb898789dd59a8cf38c660f
Exuslab <exuslab@gmail.com>
2026-02-20 18:41:19 +0800

    add 1 Two Sum

---
+++ algorithms/1 Two Sum/main.go
@@ -0,0 +1,11 @@
+func twoSum(nums []int, target int) []int {
+	seen := make(map[int]int, len(nums))
+
+	for i, v := range nums {
+		if j, ok := seen[target-v]; ok {
+			return []int{j, i}
+		}
+		seen[v] = i
+	}
+	return nil
+}
